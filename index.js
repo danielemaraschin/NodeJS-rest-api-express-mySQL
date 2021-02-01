df40c942712eda77840d37b2993e513fcd31d746
danielemaraschin <danikmaraschin@gmail.com>
2021-02-01 13:51:49 +0100

    working with get, req, res,

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const express = require('express')
 
 const app = express()
 
-app.listen(3000, ()=> console.log('Servidor rodando na porta 3000'))
+app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+
+app.get('/', (req, res) => res.send ("servidor rodando, td ok") )
+
