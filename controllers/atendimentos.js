a840bb49e7a6168a8b3beff62d4cc0d35f122c6c
danielemaraschin <danikmaraschin@gmail.com>
2021-02-01 15:10:56 +0100

    Learning about npm consign

---
+++ controllers/atendimentos.js
@@ -0,0 +1,6 @@
+
+
+module.exports = app => {
+    app.get('/atendimentos', (req, res) => res.send ("rota de atendimentos"))
+
+}
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const consign = require('consign')
 
 const app = express()
+
+consign()
+    .include('controllers')
+    .into(app)
 
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
 
 
-app.get('/atendimentos', (req, res) => res.send ("rota de atendimentos"))
+
