427746dbc8b4ec0234f5eb09f0b412a139b2b48b
danielemaraschin <danikmaraschin@gmail.com>
2021-02-01 15:18:16 +0100

    working with customExpress

---
+++ config/customExpress.js
@@ -0,0 +1,16 @@
+// as configuracoes ficam aqui
+
+const express = require('express')
+const consign = require('consign')
+
+
+module.exports = () => {
+    
+    const app = express()
+    
+    consign()
+    .include('controllers')
+    .into(app)
+
+    return app
+}
