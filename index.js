a5599840adbe06add642a8da30b4089c9bb33796
danielemaraschin <danikmaraschin@gmail.com>
2021-02-01 12:12:54 +0100

    localhost 3000

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require('express')
 
 const app = express()
 
-app.listen(300, ()=> console.log('Servidor rodando na porta 3000'))
+app.listen(3000, ()=> console.log('Servidor rodando na porta 3000'))
