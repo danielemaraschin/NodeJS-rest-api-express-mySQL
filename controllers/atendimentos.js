032eba789fb978533a7d6953cf9cc544d45173ee
danielemaraschin <danikmaraschin@gmail.com>
2021-02-02 08:10:20 +0100

    working with get

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,6 +1,4 @@
-
-
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send ("rota de atendimentos"))
+    app.get('/atendimentos', (req, res) => res.send ("rota de atendimentos realizando um get"))
 
 }
--- index.js
+++ index.js
@@ -6,3 +6,4 @@ app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
 
 
 
+
