23f7d907761d3545c7f4c63c31041d311e01ce85
danielemaraschin <danikmaraschin@gmail.com>
2021-02-02 09:20:49 +0100

    Learning about body parser

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,10 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send ("rota de atendimentos realizando um get"))
 
-    app.post('/atendimentos', (req, res) => res.send ("rota de atendimentos realizando um post"))
+    app.post('/atendimentos', (req, res) =>{
+
+        console.log(req.body)
+        
+        res.send ("rota de atendimentos realizando um post")
+    })
 }
