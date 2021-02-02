4c952213a0a2afe2cc252a6691e8174c5bd44322
danielemaraschin <danikmaraschin@gmail.com>
2021-02-02 11:49:11 +0100

    Learning about body parser

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,13 +1,19 @@
-// as configuracoes ficam aqui
+/*as configuracoes ficam nesse arquivo*/
 
+//bibliotecas externas
 const express = require('express')
 const consign = require('consign')
+const bodyParser = require('body-parser')
 
 
+//código
 module.exports = () => {
     
     const app = express()
     
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
+
     consign()
     .include('controllers')
     .into(app)
