6f70dc408ac4884eef65fd1e340fe35e69a8dadd
danielemaraschin <danikmaraschin@gmail.com>
2021-02-02 08:15:00 +0100

    working with post

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,4 +1,5 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send ("rota de atendimentos realizando um get"))
 
+    app.post('/atendimentos', (req, res) => res.send ("rota de atendimentos realizando um post"))
 }
