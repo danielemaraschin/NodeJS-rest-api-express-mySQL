7b9634d6cc3db5f03285e0f4e3798dc01f7c15f9
danielemaraschin <danikmaraschin@gmail.com>
2021-02-03 19:52:09 +0100

    working with mysql, require, .connect

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
 
-const app = customExpress()
+conexao.connect(erro => {
+    if(erro) {
+        console.log(erro)
+    } else {
+        console.log('conectado com sucesso')
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+        const app = customExpress()
 
-
-
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+    }
+})
 
