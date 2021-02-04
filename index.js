2a35d1745705a107765f9924eb798304946a25da
danielemaraschin <danikmaraschin@gmail.com>
2021-02-04 08:03:22 +0100

    Create table and fields

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const customExpress = require('./config/customExpress')
 const conexao = require('./infraestrutura/conexao')
 const Tabelas = require('./infraestrutura/tabelas')
+
 conexao.connect(erro => {
     if(erro) {
         console.log(erro)
@@ -8,10 +9,8 @@ conexao.connect(erro => {
         console.log('conectado com sucesso')
 
         Tabelas.init(conexao)
-
         const app = customExpress()
 
         app.listen(3000, () => console.log('servidor rodando na porta 3000'))
     }
 })
-
