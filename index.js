dec8e96c8e9d2731f0268501f1c6feba1396a290
danielemaraschin <danikmaraschin@gmail.com>
2021-02-04 06:39:24 +0100

    working with require

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const customExpress = require('./config/customExpress')
 const conexao = require('./infraestrutura/conexao')
-
+const Tabelas = require('./infraestrutura/tabelas')
 conexao.connect(erro => {
     if(erro) {
         console.log(erro)
