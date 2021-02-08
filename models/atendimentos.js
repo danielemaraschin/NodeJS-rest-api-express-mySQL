9898efc69c6ac763176cbd8302d203502ca7bf0f
danielemaraschin <danikmaraschin@gmail.com>
2021-02-08 09:49:21 +0100

    Working with the lib moment

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -3,19 +3,21 @@ const moment = require('moment')
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
-     adiciona(atendimento) {
-         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
-         const data = moment(atendimento.data).format('YYYY-MM-DD HH:MM:SS')
-         const atendimentoDatado = {...atendimento, dataCriacao, data}
+    adiciona(atendimento) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const atendimentoDatado = {...atendimento, dataCriacao, data}
         const sql = 'INSERT INTO Atendimentos SET ?'
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
-            if (erro) {
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+            if(erro) {
                 console.log(erro)
             } else {
-                console.log(resultados)
+               console.log(resultados)
             }
         })
     }
 }
+
+
 module.exports = new Atendimento
