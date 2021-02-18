d1f8088d23a30ac08051ff0281ef26db955e05cb
danielemaraschin <danikmaraschin@gmail.com>
2021-02-18 11:47:57 +0100

    working with create and alter table

--- exercises/addServico.js
+++ exercises/addServico.js
@@ -11,7 +11,7 @@ Considerando que este serviço deve possuir id (que será incrementado automatic
 //Como primeiro parâmetro, passamos a nossa query e, como segundo parâmetro, uma função que será executada logo após a nossa query.
 //Os valores que serão incluídos devem ser passados como parâmetro do método query, não da função de retorno.
 //O nosso Id é incrementado automaticamente, então não precisamos adicioná-lo no nosso INSERT, pois quem fará isso é o próprio SQL!
-
+//ADD essa tabela de servicos direto no mysql pq acho mais pratico
 
 conexao.query(`INSERT INTO Servicos(nome, preco) VALUES('${nome}', '${preco}')`, (erro, resultados) => {
    ...
@@ -19,6 +19,10 @@ conexao.query(`INSERT INTO Servicos(nome, preco) VALUES('${nome}', '${preco}')`,
     }
    }
 
+depois adicioneei valores e nome ao servicos (add 'nome' do servico 'banho porte medio' 'preco' '22.50' direto no mysql
+e dps alterei o valor pra '25' ao inves de fazer esse comando abaixo aqui no sublime, mas daria no mesmo )
+
+   app.put(‘atendimentos/:id’, () => { … })
    
 
 
